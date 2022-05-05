1e7a38c7a72f8756f00c05041d749cfff9fa72c0
enyasantos <enyalgs@gmail.com>
2022-05-04 23:15:00 -0300

    fixed error on authentication and added route to get user data on session

--- src/modules/authentication/repositories/IAuthenticationRepository.ts
+++ src/modules/authentication/repositories/IAuthenticationRepository.ts
@@ -2,5 +2,6 @@ import { User } from '.prisma/client';
 import { AuthDTO } from '../dtos/AuthDTO';
 
 export interface IAuthenticationRepository {
-  logon({ email, password }: AuthDTO): Promise<User>;
+  validateUser({ email, password }: AuthDTO): Promise<User>;
+  findUserById(id: string): Promise<User>;
 }
--- src/modules/authentication/services/Authentication.service.ts
+++ src/modules/authentication/services/Authentication.service.ts
@@ -1,6 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { compareSync } from 'bcrypt';
 import { AuthDTO } from '../dtos/AuthDTO';
 import AuthenticationRepository from '../infra/prisma/repositories/AuthenticationRepository';
 
@@ -14,12 +13,9 @@ export default class AuthenticationService {
   ) {}
 
   public async execute({ email, password }: AuthDTO) {
-    const user = await this.authRepository.logon({ email, password });
+    const user = await this.authRepository.validateUser({ email, password });
 
-    if (user) {
-      const isPasswordValid = compareSync(password, user.password);
-      if (!isPasswordValid) return null;
-    }
+    if (!user) return null;
 
     const payload = {
       sub: user.id,
